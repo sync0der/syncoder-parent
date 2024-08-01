bbdf4411a59be1dc0501035ccb23cec81d966c3f
sync0der <ahmadjanovinbox@gmail.com>
2024-08-02 01:24:11 +0500

    catalogue-service module added to the project

---
+++ catalogue-service/src/main/java/com/syncoder/catalogueservice/CatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syncoder.catalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueServiceApplication.class, args);
+	}
+
+}
